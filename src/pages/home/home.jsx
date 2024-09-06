a7d09dedbdeb8ef58281ecaa9bc0ddb3e0450252
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2024-09-06 15:27:44 -0500

    Se añade iluminación

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,24 +2,34 @@ import "./Home.css"
 import { Suspense, useCallback } from "react";
 import { Canvas, useLoader} from "@react-three/fiber";
 import { FirstPersonControls, FlyControls, OrbitControls } from "@react-three/drei";
-import { PerspectiveCamera } from "@react-three/drei";
 import { TextureLoader } from "three";
 
-const CustomMaterial = (props) => {
-    return <meshBasicMaterial {...props} color="red" />;
-};
 
+const StoneBox = () => {
+    const stoneTexture = useLoader
+    (TextureLoader,"./textures/stone.png")
+
+    return (
+        <mesh>
+            <boxGeometry args={[1, 1, 1]} />
+            {/* Aplicando la textura a la caja */}
+            <meshStandardMaterial map={stoneTexture} />
+        </mesh>
+    );
+}
 
 const Home = () => {
 
     return (
         <div class='container-home'>
             <Canvas>
+                <directionalLight position={[5, 5, 5]} intensity={1} />
                 <mesh>
-                    <boxGeometry args={[1,1,1]}/>
-                    <meshBasicMaterial color={"purple"}/>
-                </mesh>
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial/>
+        </mesh>
                 <FirstPersonControls/>
+                <OrbitControls/>
             </Canvas>
         </div>
 
