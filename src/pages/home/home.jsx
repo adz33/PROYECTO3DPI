f11266bb3a86d42ac2ef47bb5e715bdc122f4f04
Esteban93040 <128560724+Esteban93040@users.noreply.github.com>
2024-09-06 15:31:41 -0500

    Creacion de controles (#6)
    Co-authored-by: Esteban930402 <129241131+Esteban930402@users.noreply.github.com>

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,19 +1,25 @@
 import "./Home.css"
-import { useCallback } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Suspense, useCallback } from "react";
+import { Canvas, useLoader} from "@react-three/fiber";
+import { FirstPersonControls, FlyControls, OrbitControls } from "@react-three/drei";
+import { PerspectiveCamera } from "@react-three/drei";
+import { TextureLoader } from "three";
 
 const CustomMaterial = (props) => {
     return <meshBasicMaterial {...props} color="red" />;
-  };
+};
+
+
 const Home = () => {
 
     return (
-        <div className="Container" style={{width: '100%', height: '100%'}} >
+        <div class='container-home'>
             <Canvas>
                 <mesh>
                     <boxGeometry args={[1,1,1]}/>
                     <meshBasicMaterial color={"purple"}/>
                 </mesh>
+                <FirstPersonControls/>
             </Canvas>
         </div>
 
