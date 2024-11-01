98e3d6478090e350f418bd7099c5b61b471983f6
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2024-11-01 17:12:14 -0500

    Actulización del home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,43 +1,95 @@
 import "./home.css"
-import { Suspense, useCallback } from "react";
-import { Canvas, useLoader, useFrame} from "@react-three/fiber";
-import { FirstPersonControls, FlyControls, OrbitControls } from "@react-three/drei";
-import { TextureLoader } from "three";
 import React,{ useRef } from "react";
+import { Container, Nav, Navbar,Carousel} from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-
-const AnimatedBox = () => {
-    const meshRef = useRef();
-
-    useFrame(({ clock }) => {
-        if (meshRef.current) {
-            meshRef.current.position.y = Math.cos(clock.getElapsedTime()) * 1; // Oscila en el eje y
-        }
-    });
-
-    return (
-        <mesh ref={meshRef}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial />
-        </mesh>
-    );
-};
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
+    const handleDeforestationClick = () => {
+        navigate("/deforestation");
+    };
+
+    const handleSoilErosionClick = () => {
+        navigate("/soilErosion");
+    };
 
     return (
         
         <div class='container-home' >
-            <button class='button-logout'>
-            Cerrar Sesión
-            </button>
-            <Canvas>
-                <directionalLight position={[5, 5, 5]} intensity={1} />
-                <ambientLight intensity={0.5} />
-                <AnimatedBox/>
-
-                <FirstPersonControls/>
-            </Canvas>
+            <div className="deforestation-Navbar">
+                <Navbar className="navbar" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/home"> {/* Cambié a una ruta real */}
+                        <img
+                            alt=""
+                            src="/src/assets/SVG/Recurso 9.svg"
+                            width="40"
+                            height="40"
+                            className="d-inline-block align-top"
+                        />{' '}
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="ms-auto">
+                                <NavLink to="/" className="nav-link"> {/* Usamos to en lugar de href para redireccinar, redirige a la pagina erosión del suelo */}
+                                    Iniciar Sesión
+                                </NavLink>
+                                <NavLink to="/soilErosion" className="nav-link"> {/* Usamos to en lugar de href para redireccinar, redirige a la pagina erosión del suelo */}
+                                    Erosión del Suelo
+                                </NavLink>
+                                <NavLink to="/Deforestation" className="nav-link"> {/* Redirige a la pagina deforestación */}
+                                    Deforestación
+                                </NavLink>
+                            </Nav>
+                            <Nav className="ms-auto">
+                                <NavLink to="/Login" className="nav-link"> {/* Redirige a la página de Login */}
+                                    Cerrar Sesión
+                                </NavLink>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </div>
+            <div>
+                <Carousel >
+                    <Carousel.Item>
+                    <img
+                            className="d-block w-100"
+                            src="/src/pages/home/slides/slide.jpg"
+                            />
+                        <Carousel.Caption>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src="/src/pages/home/slides/slide-1.jpg"
+                            />
+                        <Carousel.Caption>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+            <div className="row">
+                <div className="item-deforestation">
+                    <h2> Que es la deforestación</h2>
+                    <p>La deforestación es la eliminación de bosques o áreas verdes, trasformando estas tierras para otros usus,
+                        como por ejemplo: La agricultura, ganaderia, urbanización, entre otros.
+                    </p>
+                    <p>Este proceso implica la perdida de árboles y vegetación, lo que afecta significativamente el equilibrio ecologico.</p>
+                    <button className="button-home" onClick={handleDeforestationClick}>Mas información</button>
+                </div>
+                <div className="item-soilErosion">
+                    <h2> Que es la Erosión del suelo</h2>
+                    <p>La erosión del suelo es la degradación de la capa superficial del suelo, que es rica en nutrientes y esencial para el crecimiento
+                        de las plantas. Este proceso puede ser acelerado por activididaes humanas como la deforestación, la agricultura intensiva y la construcción. </p>
+                        <button className="button-home" onClick={handleSoilErosionClick}>Mas información</button>
+                </div>
+            </div>
         </div>
 
     );
