9b5f1966f10f1c3368f31760c708b8cc414e576d
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2024-11-11 19:53:16 -0500

    Personajes puestos en escena

--- src/pages/deforestation/level0/Lights/Lighs.jsx
+++ src/pages/deforestation/level0/Lights/Lighs.jsx
@@ -4,6 +4,7 @@ import { Color } from "three";
 const Lights = () => {
     return (
         <>
+        <ambientLight intensity={1}/>
         <directionalLight position={[-10,10,0]} intensity={3}/>
         </>
     );
--- src/pages/deforestation/level0/level0.jsx
+++ src/pages/deforestation/level0/level0.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
-import { OrbitControls, useGLTF, Html, useTexture} from '@react-three/drei';
+import { OrbitControls, Html} from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import InitialMap from "./world/initialMap.jsx";
 import Lights from './Lights/Lighs.jsx';
-
+import Cientific from './Characters/Cientific.jsx';
+import MaleCharacter from "./Characters/maleCharacter.jsx";
 
 const Level0 = () => 
     {
         const [selectedCharacter, setSelectedCharacter] = useState(null);
 
+        
         return (
             <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
                 <Canvas camera={{position: [0, 1, 0] }} style={{ width: '100%', height: '100%' }}>
@@ -19,6 +21,9 @@ const Level0 = () =>
                     </Html>
                     <Lights />
                     <OrbitControls enableZoom={true} />
+                    <MaleCharacter/>
+                    <Cientific position={[0,0.3,0]} />
+                    <MaleCharacter position={[2,0.3,0]} />
                     <InitialMap />
                 </Canvas>
             </div>
