f04096392c4b37ff9cf24b56596b481146e78a68
esteban93040 <espinosa.esteban@correounivalle.edu.co>
2024-11-15 22:57:17 -0500

    Añadiendo Environment y texto html

--- src/pages/deforestation/level0/level0.jsx
+++ src/pages/deforestation/level0/level0.jsx
@@ -5,7 +5,8 @@ import InitialMap from "./world/initialMap.jsx";
 import Lights from './Lights/Lighs.jsx';
 import Cientific from './Characters/Cientific.jsx';
 import MaleCharacter from "./Characters/maleCharacter.jsx";
-
+import Environment from './Environment/Environment.jsx';
+import InGameNavBar from './Navbar/Navbar.jsx';
 
 const characterselectect = (props) => {
 
@@ -17,13 +18,17 @@ const Level0 = () =>
 
         
         return (
-            <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-                <Canvas camera={{position: [0, 1, 0] }} style={{ width: '100%', height: '100%' }}>
-                    <Html position={[1, 2, 2]}>
+            <div style={{ width: '100vw', height: '100vh', overflow: 'hidden', position: 'relative' }}>
+                <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', zIndex: 10 }}>
+                    <InGameNavBar />
+                </div>
+                <Canvas camera={{position: [-2, 3, 0] }} style={{ width: '100%', height: '100%' }}>
+                    <Html position={[15, 10, 0]}>
                         <div>
                             <h1>Bienvenido al Juego</h1>
                         </div>
                     </Html>
+                    <Environment />
                     <Lights />
                     <OrbitControls enableZoom={true} />
                     <MaleCharacter/>
